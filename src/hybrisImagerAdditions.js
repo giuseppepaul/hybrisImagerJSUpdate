ed37a8a8b23a74dcaad847df4e365f5204d2cefd
Giuseppe <giuseppe.davies@jdwilliams.co.uk>
2016-04-22 15:26:50 +0100

    Added background image support

--- src/hybrisImagerAdditions.js
+++ src/hybrisImagerAdditions.js
@@ -1,14 +1,19 @@
 Imager.prototype.init = function(){
-    var self = this;
-    this.availablePixelRatios = [2,3,5];
-	this.devicePixelRatio = this.getDevicePixelRatio();
-    var removeString = this.createPixelMediaRemoveString(this.availablePixelRatios);
+    if (!$('html').hasClass('low-res-images-only')){
+        var self = this;
+        this.availablePixelRatios = [2,3];
+    	this.devicePixelRatio = this.getDevicePixelRatio();
+        var removeString = this.createPixelMediaRemoveString(this.availablePixelRatios);
 
-    // If the device pixel density is higher than the default of 1
-    if (this.devicePixelRatio > 1){
-        // replace the data-media value with that from the appropriate pixel density attribute
+        // If the device pixel density is higher than the default of 1
+        if (this.devicePixelRatio > 1){
+            // replace the data-media value with that from the appropriate pixel density attribute
+            $(this.eles).each(function(){
+                self.replaceWithPixelDensitySrc(this, self.devicePixelRatio);
+            });
+        }
+        // Remove any pixel density attributes no longer needed
         $(this.eles).each(function(){
-            self.replaceWithPixelDensitySrc(this, self.devicePixelRatio);
             self.removePixelMediaAttr(this, removeString);
         });
     }
@@ -16,6 +21,46 @@ Imager.prototype.init = function(){
     // Original calls from hybris imager to do the setup
     this.checkImagesNeedReplacing(this.eles);
     if(this.onResize){this.registerResizeEvent();}
+};
+
+Imager.prototype.replaceImagesBasedOnScreenDimensions = function (image) {
+    var src,availableWidths = [],srcARRAY;
+    var $image = $(image);
+
+    if ($image.attr("data-media") !== undefined){
+
+        var type = "img";
+        if (image.nodeName !== "IMG") type = "bgImg";
+        var eMedia = $image.attr('data-media');
+        $image.removeAttr('data-media');
+        eMedia = $.parseJSON(eMedia)
+
+        $.each(eMedia, function(key, value) {
+            availableWidths.push(parseInt(key));
+        });
+
+        $image.data({
+            width: availableWidths,
+            media: eMedia,
+            type: type
+        });
+    }
+
+    srcARRAY = $image.data("media");
+
+    // Get width of parent for images or viewport for bg images
+    if ($image.data("type") === "img"){
+        var cwidth = Imager.getClosestValue($image.parent().width(), $.extend([],$(image).data("width")));
+        if (image.src == srcARRAY[cwidth]) return;
+        image.src = srcARRAY[cwidth];
+    } else {
+        var cwidth = Imager.getClosestValue($(window).width(), $.extend([],$(image).data("width")));
+        // Get the bg image src
+        var bgSrc = $image.css('background-image');
+        bgSrc = bgSrc.replace('url(','').replace(')','');
+        if (bgSrc === srcARRAY[cwidth]) return;
+        $image.css('background-image', 'url(' + srcARRAY[cwidth] + ')');
+    }
 };
 
 Imager.prototype.removePixelMediaAttr = function (img, attr){
@@ -38,7 +83,7 @@ Imager.prototype.replaceWithPixelDensitySrc = function (img, pixelRatio){
 };
 
 Imager.prototype.getPixelRatio = function () {
-    return window.devicePixelRatio;
+    return (window.devicePixelRatio || 1);
 };
 
 Imager.prototype.getDevicePixelRatio = function (){
@@ -46,7 +91,7 @@ Imager.prototype.getDevicePixelRatio = function (){
 };
 
 Imager.prototype.getClosestPixelRatio = function (base, available){
-    var ratio = available[0];
+    var ratio = 1; // default
     base = parseFloat(base);
     for (var i = 0; i < available.length; i++) {
         if (base >= available[i] && available[i] > ratio){
